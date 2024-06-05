4fb78a58eda8565fab24d47981348d03acaf5506
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2024-06-05 02:06:24 -0300

    bla bla bla

--- funcs/chi_cuadrado.py
+++ funcs/chi_cuadrado.py
@@ -1,19 +1,24 @@
 import numpy as np
-import scipy
-def chi_squared_test(numbers: list, num_bins=10, alpha=0.05):
-    expected_frequency = len(numbers) / num_bins
+import scipy.stats as stats
+
+def chi_squared_test(numbers: list, num_bins=10, alpha=0.005):
+    # Convertir los números a una escala de 0 a 1 si es necesario
+    scaled_numbers = [(x / max(numbers)) for x in numbers]
     
-    # Calcular el histograma de los números generados
-    observed_frequency, _ = np.histogram(numbers, bins=num_bins)
+    # Definir los límites de los bins
+    bin_edges = np.linspace(0, 1, num_bins+1)
     
-    # Calcular el estadístico de prueba (chi-cuadrado)
+    # Crear un histograma con los números escalados
+    observed_frequency, _ = np.histogram(scaled_numbers, bins=bin_edges)
+    
+    # Calcular la frecuencia esperada
+    expected_frequency = len(scaled_numbers) / num_bins
+    
+    # Calcular el estadístico chi-cuadrado
     chi_squared_statistic = np.sum((observed_frequency - expected_frequency) ** 2 / expected_frequency)
     
-    # Calcular el valor crítico de chi-cuadrado
-    critical_value = scipy.stats.chi2.ppf(1 - alpha, num_bins - 1)
+    # Obtener el valor crítico de la distribución chi-cuadrado
+    critical_value = stats.chi2.ppf(1 - alpha, num_bins - 1)
     
-    # Comparar el estadístico de prueba con el valor crítico
-    if chi_squared_statistic <= critical_value:
-        return True, chi_squared_statistic, critical_value
-    else:
-        return False, chi_squared_statistic, critical_value
+    # Retornar el resultado de la prueba
+    return chi_squared_statistic <= critical_value
